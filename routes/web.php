e7f45008f7f78cda26b32dfa3d22f88688b510f2
Prianka Mimi <priankamimi0204@gmail.com>
2025-04-17 13:15:34 +0600

    Home blade templating

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', FrontendController::class);
